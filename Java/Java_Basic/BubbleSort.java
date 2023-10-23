9eebb4cdcf38849612920e6128ca506179ddfa59
Junyoung Kim <119163732+coder-j-0090@users.noreply.github.com>
2023-10-23 00:56:23 -0700

    Add files via upload

---
+++ Java/Java_Basic/BubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BubbleSort
+{
+    public static void main(String[]args)
+    {
+        int [] data = new int[5];
+        int i,j,t;
+        Scanner scan = new Scanner(System.in);
+
+        for(i=0;i<5;i++)  					//Enter the value
+        {
+            System.out.print("Enter the value of "+i);
+            data[i] = scan.nextInt();
+        }
+        for(i=3; i>=0; i--)         //The last bubble decrease from n-1 to 0
+        {
+            for(j=0;j<=i;j++)       //Comparing a pair of bubble from first to ith.
+            // 비교하는 버블쌍은 처음부터 i번쨰 까지 비교한다.
+            {
+                if(data[j]>data[j+1])		// Compare two consecutive pair.
+                {
+                    t=data[j];				//Exchange
+                    data[j]=data[j+1];
+                    data[j+1]=t;
+                }
+            }
+        }
+        for(i=0;i<5;i++)						//Print
+            System.out.println(data[i]);
+
+    }
+}
