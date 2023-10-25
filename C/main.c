e758805504b24af528d946ea98ad23c6cc18db32
Junyoung Kim <119163732+coder-j-0090@users.noreply.github.com>
2023-10-25 13:11:11 -0700

    Add files via upload

---
+++ C/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Basic
+//
+//  Created by Junyoung Kim on 2023/10/25.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d", &a, &b);
+    c=a+b;
+    printf("%d+%d=%d", a, b, c);
+    return 0;
+}
