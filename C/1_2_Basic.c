d7a628bc3455cd99abf3202fd3727886fc72a0d9
Junyoung Kim <119163732+coder-j-0090@users.noreply.github.com>
2023-10-25 13:18:18 -0700

    Update and rename main.c to 1_2_Basic.c

---
+++ C/1_2_Basic.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  Basic
+//
+//  Created by Junyoung Kim on 2023/10/25.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+    if(a==100||b==100||c==100)
+    {
+        printf("Special Acceptance");
+    }
+    else if(a>40&&b>40&&c>40&&(a+b+c)>180)
+    {
+        printf("Acceptance");
+    }
+    else
+    {
+        printf("Fail");
+    }
+}
