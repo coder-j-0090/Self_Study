3ec151f0ed4cb001667499cb7136a3fd1bc17244
Junyoung Kim <119163732+coder-j-0090@users.noreply.github.com>
2023-10-26 20:34:18 -0700

    Update and rename Dijkstra.java to 2_Dijkstra.java

--- Java/Algorithm/2_Dijkstra.java
+++ Java/Algorithm/2_Dijkstra.java
@@ -2,7 +2,7 @@ public class Dijkstra
 {
     public static void main(String[]args)
     {
-        Graph g= new Graph();       //그래프 객체 생성
+        Graph g= new Graph();    
         g.dijkstra(1);
         g.answerPrint();
     }
