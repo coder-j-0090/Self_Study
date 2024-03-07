8dec489e00c2b5daf208c6961d4a5edf96527595
Junyoung Kim <119163732+coder-j-0090@users.noreply.github.com>
2024-03-07 01:35:56 -0800

    Add files via upload

---
+++ Project/Personal_Project/Camera_Connection/Vid_Obj_Detect_OpenCV.py
@@ -0,0 +1,45 @@
+import cv2 as cv 
+
+thres = 0.5    #Threshold to detect object
+
+cap = cv.VideoCapture(0)
+cap.set(3, 648)
+cap.set(4, 480)
+
+#classNames start from 0, not 1. So we have to subtract 1.
+classNames = []
+classFile = '/Users/junyoungkim/Desktop/CodeClause(강의)/2. Object Detection System/5. Murtaza/coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = '/Users/junyoungkim/Desktop/CodeClause(강의)/2. Object Detection System/5. Murtaza/Murtaza_Setting/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = '/Users/junyoungkim/Desktop/CodeClause(강의)/2. Object Detection System/5. Murtaza/Murtaza_Setting/frozen_inference_graph.pb'
+
+net = cv.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+
+while True:
+    success, img = cap.read()
+    #If the similarity is less than 50%, ignore.
+    classIds, confs, bbox = net.detect(img, confThreshold = thres)
+    print(classIds, bbox)
+
+
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv.rectangle(img, box, color = (0,255,0), thickness=3)
+            cv.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30), 
+                        cv.FONT_HERSHEY_COMPLEX, 1, (0,255,0), thickness = 2)
+
+    cv.imshow("Output", img)
+    cv.waitKey(1) 
+    if cv.waitKey(1) == ord('q'):
+        break 
+
+cap.release()
+cv.destroyAllWindows()
